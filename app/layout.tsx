2448dd3d0f69b634e45b5b98ee2dbe7d26cafc77
kayzar26 <sayedamaan513@gmail.com>
2026-01-09 20:39:21 +0400

    vercel analytics insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -40,6 +41,7 @@ export default function RootLayout({
           </SmoothScroller>
         </div>
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
